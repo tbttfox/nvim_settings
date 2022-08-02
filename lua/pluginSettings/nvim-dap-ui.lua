2663863e5d0923e05f36f00eacd7e4f4b8ec0074
Tyler Fox <tbttfox@gmail.com>
2022-08-02 12:27:44 -0700

    Setup the DAP and give it sane keymaps

--- lua/pluginSettings/nvim-dap-ui.lua
+++ lua/pluginSettings/nvim-dap-ui.lua
@@ -1,11 +1,17 @@
 local ret = {}
+
+vim.keymap.set("n", "<leader>du", ":lua require('dapUi').toggle()<CR>")
+
 function ret.loadPlugin(packUse)
     packUse({
         "rcarriga/nvim-dap-ui",
         requires = "mfussenegger/nvim-dap",
         config = function ()
-            require("dapui").setup({
-            })
+            local dap, dapui = require("dap"), require("dapui")
+            dapui.setup({})
+            dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+            dap.listeners.after.event_terminated["dapui_config"] = dapui.close
+            dap.listeners.after.event_exited["dapui_config"] = dapui.close
         end,
     })
 end
--- lua/pluginSettings/nvim-dap.lua
+++ lua/pluginSettings/nvim-dap.lua
@@ -1,10 +1,17 @@
 local ret = {}
+
+vim.keymap.set("n", "<F2>", ":lua require('dap').step_into()<CR>")
+vim.keymap.set("n", "<F3>", ":lua require('dap').step_out()<CR>")
+vim.keymap.set("n", "<F4>", ":lua require('dap').step_over()<CR>")
+vim.keymap.set("n", "<F5>", ":lua require('dap').continue()<CR>")
+
+vim.keymap.set("n", "<leader>b", ":lua require('dap').toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>B", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint Condition: '))<CR>")
+vim.keymap.set("n", "<leader>lp", ":lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message:'))<CR>")
+vim.keymap.set("n", "<leader>dr", ":lua require('dap').repl.open()<CR>")
 function ret.loadPlugin(packUse)
     packUse({
         "mfussenegger/nvim-dap",
-        -- config = function ()
-            -- require('dap').setup({})
-        -- end,
     })
 end
 return ret
