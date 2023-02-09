2f3ac490371cd7845aeb114db1c153507d96379c
Tyler Fox <tyler@blur.com>
2023-02-08 20:05:15 -0800

    Initial Commit

---
+++ lua/tfox/plugins/colorscheme.lua
@@ -0,0 +1,27 @@
+return {
+
+  {
+    "rktjmp/lush.nvim",
+  },
+
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+  },
+
+  -- arctic
+  {
+    "tbttfox/arctic.nvim",
+    name = "arctic",
+  },
+
+}
