d6af0f4d3ab5377a484deed1d9a4777883323603
Tyler Fox <tbttfox@gmail.com>
2023-03-07 01:41:20 -0800

    Add vim formatter

--- lua/tfox/lazyConfig/init.lua
+++ lua/tfox/lazyConfig/init.lua
@@ -21,10 +21,47 @@ return {
     },
 
     -- Python code formatting
+    --[[
     {
         "psf/black",
         init = function()
             vim.g.black_skip_string_normalization = true
+        end,
+    },
+    --]]
+    {
+        "mhartington/formatter.nvim",
+        name = "formatter",
+        config = function()
+            -- Utilities for creating configurations
+            local util = require "formatter.util"
+
+            -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+            require("formatter").setup {
+                -- Enable or disable logging
+                logging = true,
+                -- Set the log level
+                log_level = vim.log.levels.WARN,
+                -- All formatter configurations are opt-in
+                filetype = {
+                    -- Formatter configurations for filetype "lua" go here
+                    -- and will be executed in order
+                    lua = {
+                        require("formatter.filetypes.lua").stylua,
+                    },
+                    python = {
+                        require("formatter.filetypes.python").black,
+                    },
+
+                    -- Use the special "*" filetype for defining formatter configurations on
+                    -- any filetype
+                    ["*"] = {
+                        -- "formatter.filetypes.any" defines default configurations for any
+                        -- filetype
+                        require("formatter.filetypes.any").remove_trailing_whitespace
+                    }
+                }
+            }
         end,
     },
 
@@ -63,7 +100,7 @@ return {
                 "lua",
                 "markdown",
                 "markdown_inline",
-                "python",
+                --"python",
                 "query",
                 "regex",
                 "tsx",
@@ -962,7 +999,7 @@ return {
                     program = '${file}',
                     request = 'launch',
                     type = 'python',
-                    python = 'C:\\Users\\Tyler\\AppData\\Local\\Programs\\Python\\Python39\\python.exe',
+                    python = 'C:\\Users\\Tyler\\AppData\\Local\\Programs\\Python\\Python311\\python.exe',
                 }
             )
         end,
